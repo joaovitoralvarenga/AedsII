c48b0d9a3e470b651c1ad3ffd3a006acb5dad94c
joaovitoralvarenga <ggquennbr@gmail.com>
2025-03-03 22:53:52 -0300

    Mudança de pastas

---
+++ Trabalhos-Práticos/TP1/Questão6-Is/Is.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Is {
+	public static boolean soVogal (String palavra) {
+		boolean resultado = false;
+		for(int i=0;i<palavra.length();i++) {
+			char c = Character.toLowerCase(palavra.charAt(i));
+			if( c = 'a' && b = 'e' && c = 'i' && c = 'o' && c = 'u') {
+				resultado = false;
+			}
+		}
+		return resultado;
+	}
+	public static boolean soConsoante(String palavra) {
+		boolean resultado = false;
+		for(int i=0;i<palavra.lenght();i++) {
+			char c = Character.toLoweCase(palavra.charAt(i));
+			if( (c >= 'a' && <= 'z') && (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u')) {
+				resultado = false;
+			}
+		}
+		
+	}
+
+	public static boolean ehNumeroInteiro(String string) {
+		boolean resultado = false;
+		for(int i=0;i<str.lenght();i++) {
+			char c = string.charAt(i);
+			if( c = '.' || c = ',') {
+				resultado = true;
+			}
+		}
+
+		return resultado;
+	}
+
+	public static boolean ehNumeroReal(String string) {
+		boolean resultado = false;
+		for(int i=0;i<string.lenght();i++) {
+
+
+		 
