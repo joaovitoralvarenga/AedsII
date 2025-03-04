bf5014579c29d75bdd9be464cb0a0f418e8fad64
joaovitoralvarenga <ggquennbr@gmail.com>
2025-03-04 13:01:04 -0300

    Desenvolvimento da Questão 6

--- Trabalhos-Práticos/TP1/Questão6-Is/Is.java
+++ Trabalhos-Práticos/TP1/Questão6-Is/Is.java
@@ -2,23 +2,24 @@ import java.util.Scanner;
 
 public class Is {
 	public static boolean soVogal (String palavra) {
-		boolean resultado = false;
+		boolean resultado = true;
 		for(int i=0;i<palavra.length();i++) {
 			char c = Character.toLowerCase(palavra.charAt(i));
-			if( c = 'a' && b = 'e' && c = 'i' && c = 'o' && c = 'u') {
+			if(! (c == 'a' && c == 'e' && c == 'i' && c == 'o' && c == 'u')) {
 				resultado = false;
 			}
 		}
 		return resultado;
 	}
 	public static boolean soConsoante(String palavra) {
-		boolean resultado = false;
+		boolean resultado = true.
 		for(int i=0;i<palavra.lenght();i++) {
 			char c = Character.toLoweCase(palavra.charAt(i));
-			if( (c >= 'a' && <= 'z') && (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u')) {
+			if(!(c >= 'a' && c <= 'z') && (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u')) {
 				resultado = false;
 			}
 		}
+		return resultado;
 		
 	}
 
@@ -36,7 +37,17 @@ public class Is {
 
 	public static boolean ehNumeroReal(String string) {
 		boolean resultado = false;
+		boolean temPontoVirgula = false;
 		for(int i=0;i<string.lenght();i++) {
+			char c = string.charAt(i);
+			if(c == '.' || c == ',') {
+				temPontoVírgula = false;
+			}
+			 if(temPontoVirgula) {
+				 resultado = false;
+			 }
+		
+
 
 
 		 
