a4a66a3f611c035202cff83b3224f2175a9d5b77
joaovitoralvarenga <ggquennbr@gmail.com>
2025-03-08 20:04:42 -0300

    Questão 6

--- Trabalhos-Práticos/TP1/Questão6-Is/Is.java
+++ Trabalhos-Práticos/TP1/Questão6-Is/Is.java
@@ -63,8 +63,39 @@ public class Is {
 			return resultado;
 			
 			}
+			public static boolean isFim(String str) {
+				if (str.length() != 3) {
+					return false;
+				}
+				return str.charAt(0) == 'F' && str.charAt(1) == 'I' && str.charAt(2) == 'M';
+			}
+
+			public static void main(String[] args) {
+				Scanner scanner = new Scanner(System.in);
+				String entrada = scanner.nextLine().trim();
+
+				while(!isFim(entrada)) {
+					
+
+					boolean x1 = soVogal(entrada) ;
+                    boolean x2 = soConsoante(entrada);
+                    boolean x3 = ehNumeroInteiro(entrada);
+                    boolean x4 = ehNumeroReal(entrada);
+
+
+					System.out.println(
+                    (x1 ? "SIM" : "NÃO") + " " +
+                    (x2 ? "SIM" : "NÃO") + " " +
+                    (x3 ? "SIM" : "NÃO") + " " +
+                    (x4 ? "SIM" : "NÃO")
+            );
+        }
+
+		scanner.close();
+				}
+			}
 			
-		}
+		
 	
 
 
