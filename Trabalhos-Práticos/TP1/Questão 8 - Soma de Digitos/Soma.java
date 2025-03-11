062cb1a54dc24805b9088f54290f63ac67147c10
joaovitoralvarenga <ggquennbr@gmail.com>
2025-03-10 21:00:51 -0300

    Questão 8

--- Trabalhos-Práticos/TP1/Questão 8 - Soma de Digitos/Soma.java
+++ Trabalhos-Práticos/TP1/Questão 8 - Soma de Digitos/Soma.java
@@ -1,4 +1,6 @@
-import.java.util.Scanner;
+import java.util.Scanner;
+
+
 
 
 public class Soma {
@@ -6,9 +8,28 @@ public class Soma {
 	public static int SomaDigitos(int numero) {
 
 		int resultado;
-		if(numero == 0) {
-			resultado = 0;
+		if(numero < 10) {
+			resultado = numero;
+		} else {
+
+			resultado = (numero % 10) + SomaDigitos(numero/10);
 		}
+
+		return resultado;
+	}
+
+	public static boolean ehFim(String str) {
+		return(str.length() == 3 && str.charAt(0) = 'F' && str.charAt(1) = 'I' && str.charAt(2) = 'M');
+	 
+	}
+
+	public static void main(int n) {
+		Scanner scanner = new Scanner(System.in);
+		while()
+	}
+		
+
+			
 
 		
 
