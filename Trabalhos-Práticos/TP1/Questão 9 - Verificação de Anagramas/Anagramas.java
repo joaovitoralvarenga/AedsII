8c4a87d6df41a3d8f60316f7ba634bdf0ddd2b5e
joaovitoralvarenga <ggquennbr@gmail.com>
2025-03-11 01:21:53 -0300

    Questão 9

--- Trabalhos-Práticos/TP1/Questão 9 - Verificação de Anagramas/Anagramas.java
+++ Trabalhos-Práticos/TP1/Questão 9 - Verificação de Anagramas/Anagramas.java
@@ -4,42 +4,79 @@ import java.util.Scanner;
 public class Anagramas {
 
 	public static boolean ehAnagrama(String str1, String str2) {
-		bool iguais = true;
+		boolean iguais = true;
 
 			if(str1.length() != str2.length()) {
 				iguais = false;
 			}
 
-			int[] contagem = new int[52];
+
+			int[] contador = new int[26];
 
 			for(int i=0;i<str1.length();i++) {
 				char c = str1.charAt(i);
 				if(c >= 'a' && c <= 'z') {
-					contagem[c-'a']++;
+					contador[c-'a']++;
 				} else if(c >= 'A' && c <= 'Z') {
-					contador[c-'A'+26]++;
+					contador[c-'A']++;
 				}
 			}
 
 			for(int i=0;i<str2.length();i++) {
 				char c = str2.charAt(i);
 				if(c >= 'a' && c <= 'z') {
-					contador[c -'a']--;{
+					contador[c -'a']--;
 				} else if(c >= 'A' && c <= 'Z') {
-					contador[c- 'A' +26]--;
+					contador[c- 'A']--;
 	
 				}
 			}
+
+			for(int i=0;i<26;i++) {
+				if(contador[i] != 0) {
+					iguais = false;
+				}
+			}
+
+			return iguais;
+	
 		}
 
-		for(int i=0;i<52;i++) 
+		public static boolean ehFim(String str) {
+			return (str.length() == 3 && str.charAt(0) == 'F' && str.charAt(1) == 'I' && str.charAt(2) == 'M');
+		}
 
+		public static void main(String[] args) {
+			Scanner scanner = new Scanner(System.in); 
+			String entrada = scanner.nextLine();
+
+			while(!ehFim(entrada)) {
+				String str1 = "", str2 = "";
+				int i = 0;
+				while(i < entrada.length() && entrada.charAt(i)!= ' '){
+					str1 += entrada.charAt(i);
+					i++;
+				}
+
+				i += 3;
+				while(i<entrada.length()) {
+					str2 += entrada.charAt(i);
+					i++;
+				}
+				
+
+				if(ehAnagrama(str1, str2)) {
+					System.out.println("SIM");
+				} else {
+					System.out.println("NAO");
+				}
+
+				entrada = scanner.nextLine();
+			}
+
+		   scanner.close();
+		}
 		
 	}
 
-	public static boolean ehFim(String str) {
-		return (str.length() == 3 && str.charAt(0) == 'F' && str.charAt(1) == 'I' && str.charAt(2) == 'M');
-	}
-
-		  
-
+	
