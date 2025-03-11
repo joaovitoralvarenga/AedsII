c15909a4c6acf66fc0ea97c453bcf7c23bbbdb0e
joaovitoralvarenga <ggquennbr@gmail.com>
2025-03-10 23:23:01 -0300

    Questão 9

---
+++ Trabalhos-Práticos/TP1/Questão 9 - Verificação de Anagramas/Anagramas.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+public class Anagramas {
+
+	public static boolean ehAnagrama(String str1, String str2) {
+		bool iguais = true;
+
+			if(str1.length() != str2.length()) {
+				iguais = false;
+			}
+
+
+
+
+
+
+
+
+		}
+
+		public static boolean ehFim(String str) {
+			return (str.length() == 3 && str.charAt(0) == 'F' && str.charAt(1) == 'I' && str.charAt(2) == 'M');
+		}
+
+	}
+		  
+
