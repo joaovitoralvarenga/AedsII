0d71cdd8615cfbebb9b4c7d0aed829aff0230793
joaovitoralvarenga <ggquennbr@gmail.com>
2025-03-12 11:21:09 -0300

    Questão 11

--- Trabalhos-Práticos/TP1/Questão 11 - Substring mais longa/Substring.java
+++ Trabalhos-Práticos/TP1/Questão 11 - Substring mais longa/Substring.java
@@ -1,5 +1,18 @@
-import java.util.scanner;
+import java.util.Scanner;
 
 public class Substring {
 	public static String(String str) {
+
+		int repete = 0;
+
+		for(int i=0;i<str.length();i++) {
+			char letras[] = new char[str.length()];
+
+			for(int j=0;i<str.length();i++) {
+				if(letras[j] == str.charAt(i)) {
+
+					repete++
+				}
+			}
+		}
 
