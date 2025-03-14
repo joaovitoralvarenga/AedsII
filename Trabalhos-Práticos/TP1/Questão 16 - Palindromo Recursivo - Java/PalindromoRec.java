393354657a4f4ba4200d57ba43da140f889ff7f5
joaovitoralvarenga <ggquennbr@gmail.com>
2025-03-14 01:09:15 -0300

    Questão 16

--- Trabalhos-Práticos/TP1/Questão 16 - Palindromo Recursivo - Java/PalindromoRec.java
+++ Trabalhos-Práticos/TP1/Questão 16 - Palindromo Recursivo - Java/PalindromoRec.java
@@ -3,23 +3,48 @@ import java.util.Scanner;
 
 public class PalindromoRec {
 	public static boolean ehPalindromo(String str,int i) {
-		boolean palindromo = false;
-		int tamanho = str.length() /2;
+		boolean palindromo = true;
+		int tamanho = str.length() ;
 
-		if(i > tamanho) {
-			palindromo = true;	
+		if(i>tamanho/2) {
+			palindromo = true;
 		}
-
-		if(str.charAt(i) != str.charAt(tamanho - 1- i)) {
-			palindromo = false;
+		else {
+			if(str.charAt(i) != str.charAt(tamanho - 1 - i)) {
+				palindromo = false;
+			}else {
+				palindromo = ehPalindromo(str, i+1);
+			}
 		}
 
 		return palindromo;
+
+		
 	}
 	
-	public boolean ehFim(String string) {
-		return (str.length() == 3 && str.charAt()
+	public static boolean ehFim(String string) {
+		return (string.length() == 3 && string.charAt(0) == 'F' && string.charAt(1) == 'I' && string.charAt(2) == 'M');
 	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String entrada = scanner.nextLine();
+
+		while(!ehFim(entrada)) {
+			if(ehPalindromo(entrada, 0)) {
+				System.out.println("SIM");
+			} else {
+				System.out.println("NAO");
+			}
+
+			entrada = scanner.nextLine();
+		}
+
+		scanner.close();
+	}
+}
+
+	
 
 			
 		
