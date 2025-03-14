2097ef115328e3b20a825beac57474efcf703e41
joaovitoralvarenga <ggquennbr@gmail.com>
2025-03-14 00:29:38 -0300

    Questão 15

---
+++ Trabalhos-Práticos/TP1/Questão 16 - Palindromo Recursivo - Java/PalindromoRec.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+
+public class PalindromoRec {
+	public static boolean ehPalindromo(String str,int i) {
+		boolean palindromo = false;
+		int tamanho = str.length() /2;
+
+		if(i > tamanho) {
+			palindromo = true;	
+		}
+
+		if(str.charAt(i) != str.charAt(tamanho - 1- i)) {
+			palindromo = false;
+		}
+
+		return palindromo;
+	}
+	
+	public boolean ehFim(String string) {
+		return (str.length() == 3 && str.charAt()
+	}
+
+			
+		
+			
+
