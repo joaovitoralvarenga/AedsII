e00f2b667702d64f994531ea7ed455b33b174d9c
joaovitoralvarenga <ggquennbr@gmail.com>
2025-03-16 23:40:51 -0300

    Questão 2

--- Trabalhos-Práticos/TP1/Questão 02 - Palindromo - C/palindromo.c
+++ Trabalhos-Práticos/TP1/Questão 02 - Palindromo - C/palindromo.c
@@ -1,37 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <wchar.h>
+#include <stdbool.h>
 #include <locale.h>
+
+
+
 #define TAM 100
 
-int main()
-{
-    setlocale(LC_ALL, "");
-    wchar_t string[TAM];
-    
-    fgetws(string, TAM, stdin);
-    string[wcscspn(string, L"\n")] = L'\0';
-    
-    while (wcscmp(string, L"FIM") != 0) {
-        int ehPalindromo = 1;
-        int contador = wcslen(string);
-        
-        for(int i = 0; i < contador/2; i++) {
-            if(string[i] != string[contador - i - 1]){
-                ehPalindromo = 0;
-            }
+bool ehPalindromo(char string[]) {
+    bool palindromo = true;
+    int tamanho = strlen(string);
+
+    for(int i=0;i<tamanho/2;i++) {
+        if(string[i] != string[tamanho - 1 -i]) {
+            palindromo = false;
         }
-        
-        if(ehPalindromo) {
-            wprintf(L"SIM\n");
-        } else {
-            wprintf(L"NAO\n");
-        }
-        
-        fgetws(string, TAM, stdin);
-        string[wcscspn(string, L"\n")] = L'\0';
+    
     }
-    
-    return 0;
+    return palindromo;
+}
+
+int main() {
+    setlocale(LC_ALL, "pt_BR.UTF-8");
+
+    char entrada[TAM];
+
+    scanf(" ^[%\n\r]",entrada);
+    getchar();
+
+    while(strcmp(entrada, "FIM")!= 0) {
+        if(ehPalindromo(entrada)) {
+            printf("SIM\n");
+        } else {
+            printf("NAO\n");
+        }
+
+        scanf(" ^[%\n\r]",entrada);
+        getchar();
+
+        
+    }
+
+    return 0;   
+
 }
