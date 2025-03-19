ad24ca4412c8d58a87a4d8ebd04cc0c1256eb901
joaovitoralvarenga <ggquennbr@gmail.com>
2025-03-19 11:40:08 -0300

    Combinação de Strings

--- Exercícios extras/Prática Prova 1/Combinador.c
+++ Exercícios extras/Prática Prova 1/Combinador.c
@@ -5,3 +5,20 @@
 #define TAM 100
 
 
+
+void combinaStrings(char str1[], char str2[]) {
+    int j = 0;
+    char combinacao[];
+    int tamanho1 = strlen(str1);
+    int tamanho2 = strlen(str2);
+
+    for(int i=0;i<tamanho1 && i<tama2;i++) {
+        stringCombinada[j++] = str1[i];
+        stringCombinada[j++] = str2[i];
+        
+    }
+
+
+}
+
+
