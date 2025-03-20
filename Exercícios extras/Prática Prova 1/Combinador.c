e15c4952c102761312b8f2a1adc4fe447a29f84e
joaovitoralvarenga <ggquennbr@gmail.com>
2025-03-20 15:33:02 -0300

    Combinador de Strings

--- Exercícios extras/Prática Prova 1/Combinador.c
+++ Exercícios extras/Prática Prova 1/Combinador.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define TAM 100
+#define TAM 250
 
 
 
@@ -18,9 +18,36 @@ void combinaStrings(char str1[], char str2[], char combinacao[]) {
         
     }
 
-    
+    if(tamanho1 > tamanho2) {
+        for(int i=0;i<tamanho1;i++) {
+            combinacao[j++] = str1[i];
+        }
+    } else if(tamanho2> tamanho1) {
+        for(int i =0;i<tamanho2;i++) {
+            combinacao[j++] = str2[i];
+        }
 
+    }
+}
 
+int main() {
+    char str1[TAM],str2[TAM], combinacao[TAM];
+
+    while(!EOF) {
+        fgets(str1,TAM,stdin);
+        str1[strcspn(str1, "\n")] = 0;
+
+        fgets(str2,TAM,stdin);
+        str1[strcspn(str2, "\n")] = 0;
+
+        combinaStrings(str1,str2,combinacao); 
+
+        printf("%s\n", combinacao);
+
+        return 0;
+        
+
+    }
 }
 
 
