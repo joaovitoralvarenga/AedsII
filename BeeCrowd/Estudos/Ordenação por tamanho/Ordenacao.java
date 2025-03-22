15ae32a5081ed95e6025780f2c8355212934c99a
joaovitoralvarenga <ggquennbr@gmail.com>
2025-03-22 01:10:14 -0300

    Ordenação por tamanho

---
+++ BeeCrowd/Estudos/Ordenação por tamanho/Ordenacao.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+
+public class Ordenacao {
+
+
+	public static void ordenaStrings() {
+
+		Scanner scanner = new Scanner(System.in);
+
+		int n = scanner.nextInt();
+		scanner.nextLine();
+
+		for(int i=0;i<n;i++) {
+
+			String linha= scanner.nextLine();
+
+			String[] palavras = linha.split(" ");
+
+			for(int j =0;j<palavras.length;j++) {
+				int maior = j;
+
+				for(int k = j+1; k< palavras.length;k++) {
+			       if(palavras[k].length() > palavras[maior].length()) {
+				    maior = k;
+			}
+		}
+
+		String temp = palavras[j];
+		palavras[j] = palavras[maior];
+		palavras[maior] = temp;
+
+	}
+
+	
+
+	System.out.println(String.join(" ", palavras));
+	
+}
+scanner.close();
+	}
+	public static void main(String[] args) {
+		ordenaStrings();
+	}
+}
+
+
+
