b85730b37ec5d15749d95e518424819493f2f157
joaovitoralvarenga <ggquennbr@gmail.com>
2025-03-22 11:18:42 -0300

    Ordenação por tamanho- Sucesso

--- BeeCrowd/Estudos/Ordenação por tamanho/Ordenacao.java
+++ BeeCrowd/Estudos/Ordenação por tamanho/Ordenacao.java
@@ -1,35 +1,44 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Ordenacao {
-    public static void ordenaStrings() {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        scanner.nextLine(); // Consume the newline
-        
-        for(int i = 0; i < n; i++) {
-            String linha = scanner.nextLine();
-            String[] palavras = linha.split(" ");
+    public static void InsertionSort(List<String> palavras) {
+        int n = palavras.size();
+        for (int i = 1; i < n; i++) {
+            String chave = palavras.get(i);
+            int j = i - 1;
+
             
-            for(int j = 0; j < palavras.length; j++) {
-                int maior = j;
-                for(int k = j + 1; k < palavras.length; k++) {
-                    if(palavras[k].length() > palavras[maior].length()) {
-                        maior = k;
-                    }
-                }
-				if(maior != j) {
-					String temp = palavras[j];
-                palavras[j] = palavras[maior];
-                palavras[maior] = temp;
-				}
-                
+            while (j >= 0 && palavras.get(j).length() < chave.length()) {
+                palavras.set(j + 1, palavras.get(j));
+                j--;
             }
-            
+            palavras.set(j + 1, chave); 
+        }
+    }
+
+    public static void ordenaStrings() {
+
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt();
+        scanner.nextLine();
+
+        for(int i =0;i<n;i++) {
+            String linha = scanner.nextLine();
+            List<String> palavras = new ArrayList<>(Arrays.asList(linha.split(" ")));
+
+            InsertionSort(palavras);
+
             System.out.println(String.join(" ", palavras));
         }
-        
+
         scanner.close();
+    
     }
+        
     
     public static void main(String[] args) {
         ordenaStrings();
