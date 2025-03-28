0f5d6e86fa542bd7f1db9f17d807bb10d5a253b6
joaovitoralvarenga <ggquennbr@gmail.com>
2025-03-28 18:09:56 -0300

    Diamantes e Areia - 1069 - Sucesso

--- BeeCrowd/Estudos/Diamantes e Areia/DiamantesAreia.java
+++ BeeCrowd/Estudos/Diamantes e Areia/DiamantesAreia.java
@@ -4,16 +4,16 @@ import java.util.Stack;
 public class DiamantesAreia {
 
 	public static int achaDiamantes(String str) {
-		Stack<Character> diamante = new Stack<>();
-		int diamantes = 0;
+		Stack<Character> diamante = new Stack<>();             //Utilização de uma pilha, a fim de armazenar e logo retirar os elementos inseridos,
+		int diamantes = 0;                                     //assim dimunuindo o custo do processo de comparação e identificação de diamantes
 		
 		for(int i=0;i<str.length();i++) {
 			char c =str.charAt(i);
 
 			if(c == '<') {
-				diamante.push(c);
-			} else if(c == '>' && !diamante.isEmpty()) {
-				diamante.pop();
+				diamante.push(c);                          
+			} else if(c == '>' && !diamante.isEmpty()) {        //Se o elemento for ">" e a pilha conter pelo menos um elemento, ele retira esse elemento
+				diamante.pop();                                 //e incrementa a quantidade de diamantes, deixando a pilha vazia para otimizar o custo.
 				diamantes++;
 			}
 		}
