53f76b69e8b4564b75923b5068dc8297a181b8f6
joaovitoralvarenga <ggquennbr@gmail.com>
2025-03-28 15:44:05 -0300

    Diamantes e Areia- 1069

---
+++ BeeCrowd/Estudos/Diamantes e Areia/DiamantesAreia.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class DiamantesAreia {
+
+	public static int ahcaDiamantes(String str) {
+		Stack<Character> diamante = new Stack<>();
+		int diamantes = 0;
+		
+		for(int i=0;i<str.length();i++) {
+			char c =str.charAt(i);
+
+			if(c == '<') {
+				diamante.push(c);
+			} else if(c == '>' && !diamante.isEmpty()) {
+				diamante.pop();
+				diamantes++;
+			}
+		}
+
+		return diamantes;
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(Sus)
+		
+	}
+
+	
