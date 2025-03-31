456599270a5685c0ac1c32e05ca508b29392cc8b
joaovitoralvarenga <ggquennbr@gmail.com>
2025-03-31 11:45:37 -0300

    Adição de pastas exerício1

---
+++ Trabalhos-Práticos/TP2/Classe em Java/Show.java
@@ -0,0 +1,34 @@
+
+import java.util.Date;
+
+public class Show {
+	private 
+	String show_id;
+	String type;
+	String title;
+	String director;
+	String[] cast;
+	String country;
+	Date date_added;
+	int release_year;
+	String rating;
+	String duration;
+	String[]  listed_in;
+
+
+
+public Show() {
+	show_id = "";
+	type = "";
+	title = "";
+	director = "";
+	country ="";
+	
+	
+}
+
+
+
+			
+		
+		
