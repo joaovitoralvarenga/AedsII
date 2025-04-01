899c4c6086b2f112e78d502cfb4733070a22a6d5
joaovitoralvarenga <ggquennbr@gmail.com>
2025-04-01 11:56:50 -0300

    Construtores classe

--- Trabalhos-Práticos/TP2/Classe em Java/Show.java
+++ Trabalhos-Práticos/TP2/Classe em Java/Show.java
@@ -1,4 +1,5 @@
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Show {
@@ -15,6 +16,8 @@ public class Show {
 	String duration;
 	String[]  listed_in;
 
+	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
 
 
 public Show() {
@@ -23,7 +26,14 @@ public Show() {
 	title = "";
 	director = "";
 	country ="";
+	cast = null;
+	date_added = null;
+	release_year = 0;
+	rating = "";
+	duration = "";
+	listed_in = new String[0];
 	
+
 	
 }
 
