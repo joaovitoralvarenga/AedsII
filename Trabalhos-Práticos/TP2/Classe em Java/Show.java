4b001d8f299352282f720a3ee501fbb94b35dd37
joaovitoralvarenga <ggquennbr@gmail.com>
2025-04-02 10:29:33 -0300

    Desenvolvendo a classe Show

--- Trabalhos-Práticos/TP2/Classe em Java/Show.java
+++ Trabalhos-Práticos/TP2/Classe em Java/Show.java
@@ -6,7 +6,7 @@ public class Show {
 	private 
 	String show_id;
 	String type;
-	String title;
+	String title;                                
 	String director;
 	String[] cast;
 	String country;
@@ -21,23 +21,37 @@ public class Show {
 
 
 public Show() {
-	show_id = "";
-	type = "";
-	title = "";
-	director = "";
-	country ="";
+	show_id = "Nan";
+	type = "Nan";
+	title = "Nan";
+	director = "Nan";
+	country ="Nan";
 	cast = null;
 	date_added = null;
 	release_year = 0;
-	rating = "";
-	duration = "";
+	rating = "Nan";
+	duration = "Nan";
 	listed_in = new String[0];
 	
-
 	
 }
 
 
+public String getShowId() {
+	return show_id;
+}
+
+public String setShowId(Strign show_id) {
+	this.show_id = show_id;
+}
+
+public String getType() {
+	return type;
+}
+
+public String setType(String type) {
+	 this.type = type;
+}
 
 			
 		
